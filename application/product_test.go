bba6dd16f1752e9942f6d24d3fc1d0bd648ecad1
lucassantoss1701 <lucas.santoss1701@gmail.com>
2021-12-02 13:50:36 -0300

    Product created

---
+++ application/product_test.go
@@ -0,0 +1,36 @@
+package application_test
+
+import (
+	"testing"
+
+	"github.com/lucassantoss1701/hexagonal/application"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProduct_DisableEnable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "the price must be equal than zero to disable the product", err.Error())
+}
