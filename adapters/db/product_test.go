6615911a3f9521d3a75f48f3d70aa46b32110bb9
Lucas Santos <lucassantos@luizalabs.com>
2021-12-07 19:49:47 -0300

    methods save and get

---
+++ adapters/db/product_test.go
@@ -0,0 +1,78 @@
+package db_test
+
+import (
+	"database/sql"
+	"log"
+	"testing"
+
+	"github.com/lucassantoss1701/hexagonal/adapters/db"
+	"github.com/lucassantoss1701/hexagonal/application"
+	"github.com/stretchr/testify/require"
+)
+
+var Db *sql.DB
+
+func setUp() {
+	Db, _ = sql.Open("sqlite3", ":memory:")
+	createTable(Db)
+	createProduct(Db)
+}
+
+func createTable(db *sql.DB) {
+	table := `CREATE TABLE products("id" string, "name" string, "price" float, "status" string);`
+
+	stmt, err := db.Prepare(table)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	stmt.Exec()
+}
+
+func createProduct(db *sql.DB) {
+	insert := `INSERT INTO products values("abc", "teste", 0, "disabled")`
+	stmt, err := db.Prepare(insert)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	stmt.Exec()
+}
+
+func TestProductDb_Get(t *testing.T) {
+	setUp()
+	defer Db.Close()
+
+	productDb := db.NewProductDb(Db)
+	product, err := productDb.Get("abc")
+	require.Nil(t, err)
+	require.Equal(t, "teste", product.GetName())
+	require.Equal(t, 0.0, product.GetPrice())
+	require.Equal(t, "disabled", product.GetStatus())
+
+}
+
+func TestProductDb_Save(t *testing.T) {
+	setUp()
+	defer Db.Close()
+	productDb := db.NewProductDb(Db)
+
+	product := application.NewProduct()
+	product.Name = "teste"
+	product.Price = 23
+
+	productResult, err := productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+
+	product.Status = "enabled"
+
+	productResult, err = productDb.Save(product)
+	require.Nil(t, err)
+	require.Equal(t, product.Name, productResult.GetName())
+	require.Equal(t, product.Price, productResult.GetPrice())
+	require.Equal(t, product.Status, productResult.GetStatus())
+
+}
--- application/product_test.go
+++ application/product_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/lucassantoss1701/hexagonal/application"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -33,4 +34,28 @@ func TestProduct_DisableEnable(t *testing.T) {
 	product.Price = 10
 	err = product.Disable()
 	require.Equal(t, "the price must be equal than zero to disable the product", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+	product.ID = uuid.NewV4().String()
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater or equal zero", err.Error())
+
 }
